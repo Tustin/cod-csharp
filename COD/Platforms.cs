1becd96d273bfda1820a5c6fcfced538f46d04df
Josh <NGUTustin@gmail.com>
2017-12-29 17:54:47 -0600

    leaderboards, cleanup redudant code

--- COD/Platforms.cs
+++ COD/Platforms.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace COD
 {
 	public enum Platform
 	{
-		PSN,
-		XB1,
+		[Description("psn")]
+		PS4,
+
+		[Description("xb1")]
+		XboxOne,
+
+		[Description("steam")]
 		PC
 	}
 }
